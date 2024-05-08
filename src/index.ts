e6f5235af97673aae6b71fee845ad911dc29f9d3
chang313 <pch313@kaist.ac.kr>
2024-05-09 01:22:06 +0900

    wip: implement path parameter parsing

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
 import createRouter from "./router";
+import { urlParamsType } from "./router";
 
 const container: HTMLElement = document.querySelector("main") as HTMLElement;
 const pages = {
   home: () => (container.innerText = "home page"),
-  melon: () => (container.innerText = "melon page"),
+  spotify: () => (container.innerText = "spotify page"),
+  board: (params?: urlParamsType) =>
+    (container.innerText = `${params?.name} ${params?.song}`),
 };
 
 const router = createRouter();
 
-router.addRoute("#/", pages.home).addRoute("#/melon", pages.melon).start();
+router
+  .addRoute("#/", pages.home)
+  .addRoute("#/spotify", pages.spotify)
+  .addRoute("#/spotify/:name/:song", pages.board)
+  .start();
 
 window.addEventListener("click", (event: MouseEvent) => {
   const target = event.target as HTMLElement;
--- src/router.ts
+++ src/router.ts
@@ -1,20 +1,60 @@
-type routePair = { fragment: string; component: () => string };
+const ROUTE_PARAMETER_REGEXP = /:(\w+)/g;
+const URL_REGEXP = "([^\\/]+)";
+
+type routeType = {
+  fragmentRegExp: RegExp;
+  component: componentType;
+  params: string[];
+};
+
+type componentType = ((params?: urlParamsType) => string) | (() => string);
+
+export type urlParamsType = Record<string, string>;
 
 export default function createRouter() {
-  const routes: routePair[] = [];
+  const routes: routeType[] = [];
 
   const router = {
-    addRoute(fragment: string, component: () => string) {
-      routes.push({ fragment, component });
+    addRoute(fragment: string, component: componentType) {
+      const params: string[] = [];
+      const parsedFragment = fragment
+        .replace(ROUTE_PARAMETER_REGEXP, (_, paramName) => {
+          params.push(paramName);
+          return URL_REGEXP;
+        })
+        .replace(/\//g, "\\/");
+
+      routes.push({
+        fragmentRegExp: new RegExp(`^${parsedFragment}$`),
+        component,
+        params,
+      });
       return this;
     },
 
     start() {
+      const getUrlParams = (route: routeType, hash: string): urlParamsType => {
+        const params: urlParamsType = {};
+        const matches = hash.match(route.fragmentRegExp) as RegExpMatchArray;
+
+        matches.shift();
+        matches.forEach((paramValue, index) => {
+          const paramName = route.params[index];
+          params[paramName] = paramValue;
+        });
+        return params;
+      };
       const checkRoutes = () => {
-        const currentRoute = routes.find(
-          (route) => route.fragment === window.location.hash
-        ) as routePair;
-        currentRoute.component();
+        const currentRoute = routes.find((route) =>
+          route.fragmentRegExp.test(window.location.hash)
+        ) as routeType;
+
+        if (currentRoute.params.length) {
+          const urlParams = getUrlParams(currentRoute, window.location.hash);
+          currentRoute.component(urlParams);
+        } else {
+          currentRoute.component();
+        }
       };
 
       window.addEventListener("hashchange", checkRoutes);
